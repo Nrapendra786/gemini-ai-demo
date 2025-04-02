038d4b0dea80ff036e451f8135b37fb30593d4a2
Nrapendra <trivajay259@gmail.com>
2025-04-02 22:36:18 +0200

    created spring ai gemini demo project

---
+++ gemini-ai-demo/src/test/java/com/nrapendra/gemini_ai_demo/GeminiAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrapendra.gemini_ai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
